5824e4e01c190d80c8e8c8df18d11b355248020d
Wilson Law <wilsonsky18@163.com>
2017-11-22 20:23:04 +0800

    Update knn_baseline.sql

--- knn_baseline.sql
+++ knn_baseline.sql
@@ -21,26 +21,24 @@ FROM prj_tc_231620_98365_yrdets.ant_tianchi_ccf_sl_shop_info e1
 JOIN prj_tc_231620_98365_yrdets.ant_tianchi_ccf_sl_user_shop_behavior e2
 ON e1.shop_id = e2.shop_id;
 
+DROP TABLE IF EXISTS lon_la_shop_info;
 CREATE TABLE IF NOT EXISTS lon_la_shop_info
 AS
-SELECT longitude, latitude, mall_id
+SELECT longitude, latitude,mall_id,shop_id
 FROM prj_tc_231620_98365_yrdets.ant_tianchi_ccf_sl_shop_info;
 ALTER TABLE lon_la_shop_info CHANGE
-longitude shop_longitude double 
-latitude shop_latitude double
+longitude shop_longitude double;
+ALTER TABLE lon_la_shop_info CHANGE
+latitude shop_latitude double;
 
+DROP TABLE IF EXISTS jpc_knn_train;
 CREATE TABLE IF NOT EXISTS jpc_knn_train
 AS
-SELECT temp1.shop_longitude, temp1.shop_latitude, temp1.mall_id, temp2.shop_id, temp2.longitude
-	, temp2.latitude
-FROM prj_tc_231620_98365_yrdets.lon_la_shop_info temp1
-JOIN prj_tc_231620_98365_yrdets.merge_train temp2
-ON temp1.mall_id = temp2.mall_id;
+SELECT longitude, temp1.latitude, mall_id, shop_id
+FROM prj_tc_231620_98365_yrdets.merge_train; 
 
+DROP TABLE IF EXISTS jpc_knn_test;
 CREATE TABLE IF NOT EXISTS jpc_knn_test
 AS
-SELECT temp1.shop_longitude, temp1.shop_latitude, temp1.mall_id, temp2.row_id, temp2.longitude
-	, temp2.latitude
-FROM prj_tc_231620_98365_yrdets.lon_la_shop_info temp1
-JOIN prj_tc_231620_98365_yrdets.ant_tianchi_ccf_sl_test temp2
-ON temp1.mall_id = temp2.mall_id;
+SELECT longitude, latitude, mall_id
+FROM prj_tc_231620_98365_yrdets.ant_tianchi_ccf_sl_test; 
