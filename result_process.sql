aea8d825527a9089c284352e83353f1fa297d86c
Wilson Law <wilsonsky18@163.com>
2017-11-25 21:47:58 +0800

    Update result_process.sql

--- result_process.sql
+++ result_process.sql
@@ -8,8 +8,8 @@ group by shop_id,row_id;
 --选出概率最大的商店
 drop table if exists temp_1309_result;
 create table if not exists temp_1309_result as
-select row_id,shop_id from (
+select distinct(row_id),shop_id from (
 select row_id,shop_id,prob_rank from (
 select  row_id, shop_id, prob, rank() OVER (partition by row_id order by prob desc ) as prob_rank from m_1309_prob) a where prob_rank=1) b;
 select count(*) from temp_1309_result; 
-select count(distinct row_id) from prj_tc_231620_98365_yrdets.test_m_1309 ; 
+select count(distinct row_id) from test_m_1309 ; 
