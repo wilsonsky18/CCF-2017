3019a2305a265a98099377081932d4618e767e3c
Wilson Law <wilsonsky18@163.com>
2017-11-25 14:36:39 +0800

    Update result_process.sql

--- result_process.sql
+++ result_process.sql
@@ -7,4 +7,8 @@ GROUP BY shop_id,row_id;
 drop table if exists temp_1309_result;
 create table if not exists temp_1309_result as
 select row_id,max(prob) as prob from m_1309_prob
-group by row_id
+group by row_id；
+
+drop table if exists temp_1309_result;
+create table if not exists temp_1309_result as
+select row_id，shop_id from m_1309_prob where prob in(select max(prob) as prob from m_1309_prob) group by row_id,shop_id;
