77b9cc7ca3518cda2fddbdcefe641fbcbf686712
Wilson Law <wilsonsky18@163.com>
2017-11-25 11:10:26 +0800

    Create result_process.sql

---
+++ result_process.sql
@@ -0,0 +1,10 @@
+drop table if exists m_1309_prob;
+create table if not exists m_1309_prob as
+SELECT row_id, shop_id, SUM( prediction_score ) AS prob
+FROM prj_tc_231620_98365_yrdets.m_1309_result 
+GROUP BY shop_id,row_id;
+
+drop table if exists temp_1309_result;
+create table if not exists temp_1309_result as
+select row_id,max(prob) as prob from m_1309_prob
+group by row_id
