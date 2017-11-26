7bd1094854b66d731344fd946f4cd5658d1eb020
Wilson Law <wilsonsky18@163.com>
2017-11-26 09:51:54 +0800

    Update result_process.sql

--- result_process.sql
+++ result_process.sql
@@ -10,3 +10,11 @@ drop table if exists temp_1309_result;
 create table if not exists temp_1309_result_2 as
 select row_id,shop_id from(  
 select row_id, shop_id, prob, row_number () over (distribute by row_id sort by prob desc) a  from m_1309_prob) b where b.a = 1;
+
+
+--rank函数出现重复项，为解决
+drop table if exists temp_1309_result;
+create table if not exists temp_1309_result as
+select row_id,shop_id from (
+select row_id,shop_id,prob_rank from (
+select  row_id, shop_id, prob, rank() over (distribute by row_id sort by prob desc ) as prob_rank from m_1309_prob) a where prob_rank=1) b group by row_id,shop_id;
