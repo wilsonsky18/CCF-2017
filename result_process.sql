ccaf218097d7cd9b0460a5f9a5fd15650b7cd792
Wilson Law <wilsonsky18@163.com>
2017-11-26 09:50:52 +0800

    Update result_process.sql

--- result_process.sql
+++ result_process.sql
@@ -2,14 +2,11 @@
 drop table if exists m_1309_prob;
 create table if not exists m_1309_prob as
 select row_id, shop_id, SUM( prediction_score ) prob
-from prj_tc_231620_98365_yrdets.m_1309_result 
+from m_1309_result 
 group by shop_id,row_id;
 
 --选出概率最大的商店
 drop table if exists temp_1309_result;
-create table if not exists temp_1309_result as
-select distinct(row_id),shop_id from (
-select row_id,shop_id,prob_rank from (
-select  row_id, shop_id, prob, rank() over (partition by row_id order by prob desc ) as prob_rank from m_1309_prob) a where prob_rank=1) b;
-select count(*) from temp_1309_result; 
-select count(distinct row_id) from test_m_1309 ; 
+create table if not exists temp_1309_result_2 as
+select row_id,shop_id from(  
+select row_id, shop_id, prob, row_number () over (distribute by row_id sort by prob desc) a  from m_1309_prob) b where b.a = 1;
