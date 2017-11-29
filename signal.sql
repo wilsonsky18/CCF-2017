f2b171e8468d627951bb99e778c4cb1bc9ee5a8e
Wilson Law <wilsonsky18@163.com>
2017-11-29 17:32:01 +0800

    Update signal.sql

--- signal.sql
+++ signal.sql
@@ -1,13 +1,15 @@
+--一个mall测试版
 drop table if exists m_1060_wifi;
 create table if not exists m_1060_wifi as
 select wifi_id, shop_id, count(signal) num from(
 select user_id, shop_id, wifi_id,signal from(select user_id, shop_id, wifi_id,signal, rank () over (distribute by user_id,shop_id sort by signal asc) a  from train_m_1006) b 
 where b.a=1) c group by wifi_id,shop_id;
 
-drop table if exists a_jpc_wifi ;
-create table if not exists a_jpc_wifi as
+--所有mall完整版
+drop table if exists jpc_wifi_train ;
+create table if not exists jpc_wifi_train as
 select wifi_id, shop_id, count(signal) num from(
 select user_id, shop_id, wifi_id,signal from(select user_id, shop_id, wifi_id,signal, rank () over (distribute by user_id,shop_id sort by signal asc) a  from jpc_second_train) b 
 where b.a=1) c group by wifi_id,shop_id;
-select count(*) from a_jpc_wifi;
-select count(distinct wifi_id) from a_jpc_wifi;
+select count(*) from jpc_wifi_traini;
+select count(distinct wifi_id) from jpc_wifi_traini;
