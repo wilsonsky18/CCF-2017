42f5e10598906283d4583b887db351c2e0e300de
Wilson Law <wilsonsky18@163.com>
2017-11-30 21:48:30 +0800

    Update wifi_notnull_rule.sql

--- wifi_notnull_rule.sql
+++ wifi_notnull_rule.sql
@@ -1,14 +1,14 @@
 --不过滤wifi规则
-drop table if exists jpc_wifi_train ;
+drop table if exists jpc_wif_notnull_train ;
 create table if not exists jpc_wifi_train as
 select wifi_id, shop_id, count(signal) num from(
-select user_id, shop_id, wifi_id,signal from(select user_id, shop_id, wifi_id,signal, rank () over (distribute by user_id,shop_id sort by signal asc) a  from jpc_train) b 
+select user_id, shop_id, wifi_id,signal from(select user_id, shop_id, wifi_id,signal, rank () over (distribute by user_id,shop_id sort by signal asc) a  from jpc_train where signal != 'null') b 
 where b.a=1) c group by wifi_id,shop_id;
 
-drop table if exists jpc_wifi_test_temp;
-create table if not exists jpc_wifi_test_temp as
+drop table if exists jpc_wifi_notnull_test_temp;
+create table if not exists jpc_wifi_notnull_test_temp as
 select row_id, shop_id,wifi_id,num,row_number() over (partition by row_id order by num desc) rank_wifi from(
-select temp2.row_id,temp1.wifi_id,temp1.shop_id,temp1.num from jpc_wifi_train temp1 join jpc_test temp2 on temp1.wifi_id=temp2.wifi_id) temp3;
+select temp2.row_id,temp1.wifi_id,temp1.shop_id,temp1.num from jpc_wifi_train temp1 join jpc_test temp2 where temp2.signal != 'null' on temp1.wifi_id=temp2.wifi_id) temp3;
 drop table if exists jpc_wifi_test ;
 create table if not exists jpc_wifi_test as
 select row_id,shop_id from jpc_wifi_test_temp where rank_wifi=1;
