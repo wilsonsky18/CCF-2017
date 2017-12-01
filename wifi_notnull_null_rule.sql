cf99d9c8020b7987628c35db6af816d96e169eba
Wilson Law <wilsonsky18@163.com>
2017-12-01 19:30:39 +0800

    Update wifi_notnull_null_rule.sql

--- wifi_notnull_null_rule.sql
+++ wifi_notnull_null_rule.sql
@@ -84,23 +84,47 @@ select count(*) from  ant_tianchi_ccf_sl_predict; --2402119
 
 
 
+--不过滤wifi规则
+drop table if exists jpc_wifi_notnull_test;
+create table if not exists jpc_wifi_notnull_test as
+select * from jpc_test where signal != 'null';
+
+drop table if exists jpc_wifi_null_train;
+create table if not exists jpc_wifi_null_train as
+select * from jpc_train where signal = 'null';
+
+drop table if exists jpc_wifi_null_test;
+create table if not exists jpc_wifi_null_test as
+select * from jpc_test where signal = 'null';
+
+--notnull wifi计算，按强度为标准
+drop table if exists jpc_wifi_notnull_rule ;
+create table if not exists jpc_wifi_notnull_rule as
+select wifi_id, shop_id, count(signal) num from(
+select user_id, shop_id, wifi_id,signal from(select user_id, shop_id, wifi_id,signal, rank () over (distribute by user_id,shop_id sort by signal asc) a  from jpc_train where signal != 'null') b 
+where b.a=1) c group by wifi_id,shop_id;
+
+drop table if exists jpc_wifi_notnull_test_temp;
+create table if not exists jpc_wifi_notnull_test_temp as
+select row_id, shop_id,wifi_id,num,row_number() over (partition by row_id order by num desc) rank_wifi from(
+select temp2.row_id,temp1.wifi_id,temp1.shop_id,temp1.num from jpc_wifi_notnull_rule temp1 join jpc_wifi_notnull_test temp2 on temp1.wifi_id=temp2.wifi_id) temp3;
+
+drop table if exists jpc_wifi_notnull_test_result ;
+create table if not exists jpc_wifi_notnull_test_result as
+select row_id,shop_id from jpc_wifi_notnull_test_temp where rank_wifi=1;
+
+select count(distinct row_id) from  jpc_wifi_notnull_test;  -- 1519961
+select count(*) from  jpc_wifi_notnull_test_result where shop_id is not null;   -- 1505285
 
 --null wifi 按搜索到次数排序
-drop table if exists jpc_wifi_null_rule;
-create table if not exists jpc_wifi_null_rule as
-select shop_id,wifi_id,wifi_num from (
-select shop_id,wifi_id,wifi_num, rank() over(partition by shop_id order by wifi_num desc) wifi_rank from(
-select shop_id,wifi_id,count(wifi_id) wifi_num from jpc_wifi_null_train group by wifi_id,shop_id) temp) temp1 where wifi_rank = 1;
-
-select count(distinct wifi_id) from jpc_wifi_null_rule; --290715 distinct wifi_id
-select count(*) from jpc_wifi_null_rule; --331801
-
 drop table if exists jpc_wifi_null_test_result;
 create table if not exists jpc_wifi_null_test_result as
 select row_id,shop_id from (
-select temp2.row_id,temp1.wifi_id,temp1.shop_id,temp1.wifi_num from jpc_wifi_null_rule temp1 join jpc_wifi_null_test  temp2 on temp1.wifi_id=temp2.wifi_id) temp3;
+select row_id,shop_id, rank() over(partition by row_id order by wifi_num desc) rank_wifi from (
+select temp2.row_id,temp1.wifi_id,temp1.shop_id,temp1.wifi_num from jpc_wifi_null_rule temp1 join jpc_wifi_null_test  temp2 on temp1.wifi_id=temp2.wifi_id) temp3)
+temp4 where rank_wifi=1;
 select count(distinct row_id) from jpc_wifi_null_test;   --882158
-select count(*) from jpc_wifi_null_test_result where shop_id is not null; --851571
+select count(*) from jpc_wifi_null_test_result where shop_id is not null; --715117
 
 --union null和notnull的结果
 drop table if exists union_null_notnull_result;
@@ -112,9 +136,10 @@ select * from prj_tc_231620_98365_yrdets.jpc_wifi_null_test_result b;
 --生成预测结果
 drop table if exists ant_tianchi_ccf_sl_predict;
 create table if not exists ant_tianchi_ccf_sl_predict  as
-select row_id,shop_id from
+select row_id,shop_id from (
+select row_id,shop_id,row_number() over(partition by row_id order by shop_id) row_num from
 (select temp2.row_id,temp1.shop_id from union_null_notnull_result temp1 right outer join test_row_id temp2
-on temp1.row_id=temp2.row_id) temp;
+on temp1.row_id=temp2.row_id) temp ) temp1 where row_num=1;
 
 select count(*) from  union_null_notnull_result; --1972392
 select count(distinct row_id) from ant_tianchi_ccf_sl_predict result where result.shop_id is null; --429727
